7076a08f2af7850e9319d4cb796120be8e9a136a
afloresep <afloresep01@gmail.com>
2026-02-26 16:01:09 +0100

    add chemistry utils

---
+++ src/tmap/utils/__init__.py
@@ -0,0 +1,5 @@
+"""Chemistry and data utilities for TMAP."""
+
+from tmap.utils.chemistry import fingerprints_from_smiles, molecular_properties
+
+__all__ = ["fingerprints_from_smiles", "molecular_properties"]
