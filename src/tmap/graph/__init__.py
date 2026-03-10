55579f5e677cdeb86e913e7b643e4d94a0ebd9f3
afloresep <afloresep01@gmail.com>
2026-03-10 18:08:28 +0100

    Improve base template: sorted categorical legends, no copy buttons on layout columns, card links above structure, theme toggle changes scatterplot background; add graph analysis module

--- src/tmap/graph/__init__.py
+++ src/tmap/graph/__init__.py
@@ -1,6 +1,23 @@
-"""Graph module: MST construction and tree data structures."""
+"""Graph module: MST construction, tree data structures, and analysis."""
 
+from tmap.graph.analysis import (
+    boundary_edges,
+    confusion_matrix_from_tree,
+    edge_delta,
+    node_diversity,
+    path_properties,
+    subtree_purity,
+)
 from tmap.graph.mst import MSTBuilder
 from tmap.graph.types import Tree
 
-__all__ = ["MSTBuilder", "Tree"]
+__all__ = [
+    "MSTBuilder",
+    "Tree",
+    "boundary_edges",
+    "confusion_matrix_from_tree",
+    "edge_delta",
+    "node_diversity",
+    "path_properties",
+    "subtree_purity",
+]
